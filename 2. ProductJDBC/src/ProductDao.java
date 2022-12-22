676d9c1a2b77496a1497cba1329e43426881bbb3
Suyeon Lee <lsy7674@gmail.com>
2022-12-21 21:26:40 -0500

    JDBC select, insert, update, delete with Oracle

---
+++ 2. ProductJDBC/src/ProductDao.java
@@ -0,0 +1,288 @@
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ProductDao {
+
+
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url ="jdbc:oracle:thin:@localhost:1521:orcl";
+	String id="jspid";
+	String pw="jsppw";
+
+	Connection conn;
+	ResultSet rs;
+	PreparedStatement ps;
+
+	ProductDao() {
+		System.out.println("ProductDao Constructor");
+
+		try {
+			Class.forName(driver);
+			System.out.println("Driver load success");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver load failure");
+		}
+
+	} //constructor
+
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch (SQLException e) {
+			System.out.println("Connection failure");
+		}
+
+	}//getConnection
+
+	public ArrayList<ProductBean> getAllProducts() {
+		getConnection();
+
+		ArrayList<ProductBean> list = new ArrayList<ProductBean>();
+
+
+		String sql = "Select * from products order by id asc";
+		try {
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int stock = rs.getInt("stock");
+				int price = rs.getInt("price");
+				String category = rs.getString("category");
+				//String inputdate = rs.getString("inputdate");
+				Date d = rs.getDate("inputdate"); //Date Type
+				String s = String.valueOf(d); //String type
+
+
+				ProductBean pb = new ProductBean();
+				pb.setId(id);
+				pb.setName(name);
+				pb.setStock(stock);
+				pb.setPrice(price);
+				pb.setCategory(category);
+				pb.setInputdate(s);
+
+				list.add(pb);
+
+			}
+
+		} catch (SQLException e) {
+		} finally {
+			try {
+				rs.close();
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+
+			}
+		}
+
+		return list;
+
+
+	}//getAllProducts();
+
+	public ArrayList<ProductBean> getProductById(int id) {
+		getConnection();
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+
+		ArrayList<ProductBean> list = new ArrayList<ProductBean>();
+
+		String sql = "select * from products where id=?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				int id2 = rs.getInt("id");
+				String name = rs.getString("name");
+				int stock = rs.getInt("stock");
+				int price = rs.getInt("price");
+				String category = rs.getString("category");
+				//String inputdate = rs.getString("inputdate");
+				Date d = rs.getDate("inputdate"); //Date Type
+				String s = String.valueOf(d); //String type
+
+				ProductBean pb = new ProductBean();
+				pb.setId(id2);
+				pb.setName(name);
+				pb.setStock(stock);
+				pb.setPrice(price);
+				pb.setCategory(category);
+				pb.setInputdate(s);
+
+				list.add(pb);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Connection failure");
+		} finally {
+			try {
+				rs.close();
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+		return list;
+	}
+
+	public ArrayList<ProductBean> getProductsByCategory(String cate) {
+		getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<ProductBean> list = new ArrayList<ProductBean>();
+
+		String sql = "select * from products where upper(category)=?";
+
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, cate.toUpperCase());
+
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int stock = rs.getInt("stock");
+				int price = rs.getInt("price");
+				String category = rs.getString("category");
+				//String inputDate = rs.getString("inputDate");
+				Date d = rs.getDate("inputdate");
+				String s = String.valueOf(d);
+
+				ProductBean pb = new ProductBean();
+				pb.setId(id);
+				pb.setName(name);
+				pb.setStock(stock);
+				pb.setPrice(price);;
+				pb.setCategory(category);
+				pb.setInputdate(s);
+
+				list.add(pb);
+
+			}
+
+		} catch (SQLException e) {
+		} finally {
+			try {
+				rs.close();
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		return list;
+
+	}
+
+	public int insertData(ProductBean pb) {
+		//System.out.println(pb.getName());
+		//System.out.println(pb.getCategory());
+		
+		getConnection();
+		PreparedStatement ps = null;
+		int cnt = -1;
+		
+		String sql = "insert into products values(perseq.nextval, ?, ?, ?, ?, ?)";
+		
+		 try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, pb.getName());
+			ps.setInt(2, pb.getStock());
+			ps.setInt(3,  pb.getPrice());
+			ps.setString(4,  pb.getCategory());
+			ps.setString(5, pb.getInputdate());
+			
+			 cnt = ps.executeUpdate();
+			 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} //finally
+		
+		 return cnt;
+	}
+
+	public int deleteData(int id) {
+		getConnection();
+		PreparedStatement ps = null;
+		int cnt = -1;
+		String sql = "delete from products where id=?";
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			
+			 cnt = ps.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		return cnt;
+	}
+
+	public int updateData(ProductBean pb) {
+		getConnection();
+		PreparedStatement ps = null;
+		int cnt = -1;
+		
+		String sql = "update products set name=?, stock=?, price=?, category=?, inputdate=? where id=?";
+		
+		try {
+			conn.setAutoCommit(false); 
+			
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, pb.getName());
+			ps.setInt(2, pb.getStock());
+			ps.setInt(3,  pb.getPrice());
+			ps.setString(4, pb.getCategory());
+			ps.setString(5,  pb.getInputdate());
+			ps.setInt(6, pb.getId());
+			
+			 cnt = ps.executeUpdate();
+			 
+			 conn.commit();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return cnt;
+	}
+	
+}
---
+++ 2. ProductJDBC/src/ProductMain.java
@@ -0,0 +1,155 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProductMain {
+
+	Scanner sc = new Scanner(System.in);
+	ProductDao pdao = new ProductDao();
+	
+	ProductMain() {
+		init();
+	}
+	
+	
+	private void init() {
+		while(true) {
+			System.out.println("===== Menu =====");
+			System.out.println("1. Display all products");
+			System.out.println("2. Display products with ID");
+			System.out.println("3. Display products with category");
+			System.out.println("4. Update Products");
+			System.out.println("5. Product delete");
+			System.out.println("6. Product add");
+			System.out.println("7. Program exit");
+			System.out.print("Enter number >>");
+			int menu = sc.nextInt();
+			
+			switch(menu) {
+			case 1:
+				ArrayList<ProductBean> list = pdao.getAllProducts();
+				showProducts(list);
+				break;
+			case 2:
+				getProductsById();
+				break; 
+			case 3:
+				getProductsByCategory();
+				break;
+			case 4:
+				deleteData();
+				break;
+			case 5:
+				updateData();
+				break;
+			case 6:
+				insertData();
+				break;
+			case 7:
+				System.out.println("Program exits...");
+				System.exit(0);
+			default: System.out.println("Only numbers 1 to 7 can be entered.");
+			}
+		}
+	} //init
+
+
+
+	private void updateData() {
+		System.out.print("Enter the ID you want to update: ");
+		int id = sc.nextInt();
+		
+		System.out.print("Enter new name: ");
+		String name = sc.next();
+		
+		System.out.print("Enter new stock: ");
+		int stock = sc.nextInt();
+		
+		System.out.print("Enter new price: ");
+		int price = sc.nextInt();
+		
+		System.out.print("Enter new category: ");
+		String category = sc.next();
+		
+		System.out.print("Enter new date:(YYYY-MM-DD) ");
+		String date = sc.next();
+		
+		ProductBean pb = new ProductBean(id, name, stock, price, category, date);
+		int cnt = pdao.updateData(pb);
+		if(cnt == -1) {
+			System.out.println("Update failure");
+		}else  {
+			System.out.println("Update success");
+		}
+		
+	}
+
+
+	private void deleteData() {
+		System.out.print("Enter the ID you want to delete: ");
+		int id = sc.nextInt();
+		int cnt = pdao.deleteData(id);
+		if(cnt == -1) {
+			System.out.println("Delete failure");
+		} else {
+			System.out.println("Delete success");
+		}
+		
+	}
+
+	private void insertData() {
+		System.out.println("ID will be entered by sequence");
+		System.out.print("Enter the name: ");
+		String name = sc.next();
+		
+		System.out.print("Enter the stock: ");
+		int stock = sc.nextInt();
+		
+		System.out.print("Enter the price: ");
+		int price = sc.nextInt();
+		
+		System.out.print("Enter the category: ");
+		String category = sc.next();
+		
+		System.out.print("Enter the date:(YYYY-MM-DD) ");
+		String date = sc.next();
+		
+		
+		ProductBean pb = new ProductBean(0, name, stock, price, category, date);
+		int cnt = pdao.insertData(pb);
+		if(cnt == -1) {
+			System.out.println("failure in SQL execution");
+		}else {
+			System.out.println("Update success");
+		}
+		
+	}
+
+	private void getProductsByCategory() {
+		System.out.print("Enter the category you are looking for: ");
+		String cate = sc.next();
+		ArrayList<ProductBean> list = pdao.getProductsByCategory(cate);
+		showProducts(list);
+	}
+
+	private void getProductsById() {
+		System.out.print("Enter the ID you are looking for: ");
+		int id = sc.nextInt();
+		ArrayList<ProductBean> list = pdao.getProductById(id);
+		showProducts(list);
+	}
+
+	private void showProducts(ArrayList<ProductBean> list) {
+		System.out.println("Id\tName\tStock\tprice\tCategory\tInputDate");
+		for(ProductBean pb : list) {
+			System.out.println(pb.getId()+"\t"+pb.getName()+"\t"+pb.getStock()+"\t"+pb.getPrice()+"\t"+pb.getCategory()+"\t"+pb.getInputdate());
+		}		
+	}
+
+	public static void main(String[] args) {
+
+		new ProductMain();
+		
+	
+	}
+
+}
